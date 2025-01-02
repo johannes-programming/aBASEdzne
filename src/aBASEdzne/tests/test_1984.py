c5d5867749f15261a3c906aabb30255d95f93eb1
Johannes <johannes-programming@mailfence.com>
2025-01-02 13:09:54 +0100

    Version 2.0.0.dev2

--- src/aBASEdzne/tests/test_1984.py
+++ src/aBASEdzne/tests/test_1984.py
@@ -8,3 +8,7 @@ class Test1984(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+
+# python3 aBASE.py --hchain "G4:G300" --heavykeys "H3:W3" --kchain "Y4:Y300" --kappakeys "Z3:AM3"  --lchain "AO4:AO300" --lambdakeys "AP3:BC3" --dataprefix=/data/SeqData/ /data/aBASE-113-input.xlsx /results/aBASE-output.xlsm
+# echo "aBASE validation complete."
